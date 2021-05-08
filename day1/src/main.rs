81e84d3726c5749d9b79f31739c484295c98db88
RyanFrazier <rfrazier716@gmail.com>
2021-05-07 21:14:21 -0400

    cleaned up day 1 messing with clippy suggestions

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
-fn part_one(input: &str) -> Result<i32, String> {
+fn part_one(input: &str) -> i32 {
     // iterate over the input string, adding 1 for ( and -1 for )
-    input.chars().try_fold(0, |acc, char| match char {
-        '(' => Ok(acc + 1),
-        ')' => Ok(acc - 1),
-        x => Err(format!("Received Invalid Character {}", x)),
+    input.chars().fold(0, |acc, char| match char {
+        '(' => acc + 1,
+        ')' => acc - 1,
+        _ => acc,
     })
 }
 
-fn part_two(input: &str) -> Result<usize, String> {
+fn part_two(input: &str) -> usize {
     // iterate over the input string, adding 1 for ( and -1 for )
     let mut it = input.chars().enumerate();
     it.try_fold(0, |acc: u32, (_, char)| match char {
@@ -19,16 +19,16 @@ fn part_two(input: &str) -> Result<usize, String> {
     });
 
     // get the next iterator index, raise error if one never existed
-    Ok(match it.next() {
+    match it.next() {
         Some((n, _)) => n,
         None => input.len(),
-    })
+    }
 }
 
 fn main() {
     let input = fs::read_to_string("day1/input.txt").expect("Error While Reading File");
-    println!("Part 1: {}", part_one(&input).unwrap());
-    println!("Part 1: {}", part_two(&input).unwrap());
+    println!("Part 1: {}", part_one(&input));
+    println!("Part 1: {}", part_two(&input));
 }
 
 #[cfg(test)]
@@ -37,14 +37,14 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one("(())").unwrap(), 0);
-        assert_eq!(part_one("))(((((").unwrap(), 3);
-        assert_eq!(part_one(")())())").unwrap(), -3);
+        assert_eq!(part_one("(())"), 0);
+        assert_eq!(part_one("))((((("), 3);
+        assert_eq!(part_one(")())())"), -3);
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_two("()())").unwrap(), 5);
-        assert_eq!(part_two(")").unwrap(), 1);
+        assert_eq!(part_two("()())"), 5);
+        assert_eq!(part_two(")"), 1);
     }
 }
