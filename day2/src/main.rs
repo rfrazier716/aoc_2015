9b1c3058da8ae4a40c8d6de42ef375126f2cc33d
RyanFrazier <rfrazier716@gmail.com>
2021-05-07 21:08:00 -0400

    separated present implementation Have Shape3D and Present

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,46 +3,60 @@ use std::process;
 use std::str::FromStr;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Present {
+pub struct Cuboid {
     x: i32,
     y: i32,
     z: i32,
 }
 
-impl Present {
+pub trait Shape3D {
+    fn surface_area(&self) -> i32;
+    fn volume(&self) -> i32;
+}
+
+pub trait Present {
+    fn wrapping_area(&self) -> i32;
+    fn ribbon_length(&self) -> i32;
+}
+
+impl Cuboid {
     pub fn new(x: i32, y: i32, z: i32) -> Self {
         let mut sides = vec![x, y, z];
-        sides.sort();
+        sides.sort_unstable();
         Self {
             x: sides[0],
             y: sides[1],
             z: sides[2],
         }
     }
+}
 
-    pub fn surface_area(&self) -> i32 {
+impl Shape3D for Cuboid {
+    fn surface_area(&self) -> i32 {
         2 * (self.x * (self.y + self.z) + self.y * self.z)
     }
 
-    pub fn wrapping_area(&self) -> i32 {
+    fn volume(&self) -> i32 {
+        self.x * self.y * self.z
+    }
+}
+
+impl Present for Cuboid {
+    fn wrapping_area(&self) -> i32 {
         self.surface_area() + self.x * self.y
     }
 
-    pub fn volume(&self) -> i32 {
-        self.x * self.y * self.z
-    }
-
-    pub fn ribbon_length(&self) -> i32 {
+    fn ribbon_length(&self) -> i32 {
         2 * (self.x + self.y) + self.volume()
     }
 }
 
-impl FromStr for Present {
+impl FromStr for Cuboid {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let elements = s
-            .split("x")
+            .split('x')
             .map(|x| x.parse::<i32>())
             .collect::<Result<Vec<_>, _>>();
 
@@ -56,18 +70,18 @@ impl FromStr for Present {
     }
 }
 
-fn load_presents(input: &str) -> Result<Vec<Present>, String> {
+fn load_presents(input: &str) -> Result<Vec<Cuboid>, String> {
     input
         .split_terminator("\r\n")
-        .map(|s| Present::from_str(s))
+        .map(|s| Cuboid::from_str(s))
         .collect::<Result<Vec<_>, _>>()
 }
 
-fn part_one(presents: &[Present]) -> i32 {
+fn part_one(presents: &[Cuboid]) -> i32 {
     presents.iter().map(|x| x.wrapping_area()).sum::<i32>()
 }
 
-fn part_two(presents: &[Present]) -> i32 {
+fn part_two(presents: &[Cuboid]) -> i32 {
     presents.iter().map(|x| x.ribbon_length()).sum::<i32>()
 }
 
@@ -92,26 +106,26 @@ mod tests {
 
     #[test]
     fn test_present() {
-        let present = Present::new(2, 3, 4);
+        let present = Cuboid::new(2, 3, 4);
         assert_eq!(present.wrapping_area(), 58);
 
-        let present = Present::new(1, 1, 10);
+        let present = Cuboid::new(1, 1, 10);
         assert_eq!(present.wrapping_area(), 43);
     }
 
     #[test]
     fn test_present_parsing() {
         let present_string = "29x13x26";
-        let present = Present::from_str(present_string).unwrap();
-        assert_eq!(present, Present::new(29, 13, 26));
+        let present = Cuboid::from_str(present_string).unwrap();
+        assert_eq!(present, Cuboid::new(29, 13, 26));
     }
 
     #[test]
     fn test_ribbon_length() {
-        let present = Present::new(2, 3, 4);
+        let present = Cuboid::new(2, 3, 4);
         assert_eq!(present.ribbon_length(), 34);
 
-        let present = Present::new(1, 1, 10);
+        let present = Cuboid::new(1, 1, 10);
         assert_eq!(present.ribbon_length(), 14);
     }
 }
