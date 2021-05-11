4b8faf8cbca96274e90cb78c8e8ad897dd254629
rfrazier <rfrazier716@gmail.com>
2021-05-11 17:02:14 -0400

    cleaned up to use the crypto crate

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,27 +1,24 @@
-use md5;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+use std::thread;
 
-fn is_valid_checksum(base: &str, number: i32, n_zeros: usize) -> bool{
-    let checksum =  format!("{:x}",md5::compute(format!("{}{}",base,number)));
-    checksum
-    .chars()
-    .take(n_zeros)
-    .fold(true, |same, x| same && (x=='0'))
-}
-
-fn part_one(puzzle_input: &str) -> i32{
-    (0..)
-    .find(|&x| is_valid_checksum(puzzle_input, x, 5)).unwrap()
-}
-
-fn part_two(puzzle_input: &str) -> i32{
-    (0..)
-    .find(|&x| is_valid_checksum(puzzle_input, x, 6)).unwrap()
+fn find_valid_checksum(base: &str, search_key: &str, starting_index: u64) -> u64{
+    let mut sh = Md5::new(); //make a new md5 hash
+    (starting_index..)
+    .find(|&x| {
+        sh.input_str(&format!("{}{}",base, x)); // make our hash
+        let out_hash = sh.result_str();
+        sh.reset(); // reset the hash
+        out_hash.starts_with(search_key)
+    }).unwrap()
 }
 
 fn main() {
     let input = "iwrupvqb";
-    println!("Part One: {}", part_one(input));
-    println!("Part One: {}", part_two(input));
+    let part_one_soln = find_valid_checksum(input, "00000",0);
+    println!("Part One: {}", part_one_soln);
+    let part_two_soln = find_valid_checksum(input, "000000", part_one_soln);
+    println!("Part One: {}", part_two_soln);
 
 }
 
@@ -31,10 +28,7 @@ mod tests{
 
     #[test]
     fn test_valid_checksum(){
-        assert!(is_valid_checksum("abcdef",609043, 5));
-        assert!(is_valid_checksum("pqrstuv",1048970, 5));
-        assert!(is_valid_checksum("iwrupvqb",346386, 5));
-
+        assert_eq!(find_valid_checksum("abcdef","00000",609043),609043);
     }
 
     // #[test]
