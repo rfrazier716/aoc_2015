bb1592f75e9bd9380864691fafa055d5aedaa146
RyanFrazier <rfrazier716@gmail.com>
2021-05-16 08:50:33 -0400

    Day 7 Finished

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error, str::FromStr};
+use std::{collections::HashMap, error::Error, fs, process, str::FromStr};
 use regex::Regex;
 use lazy_static::lazy_static;
 
@@ -19,14 +19,14 @@ enum Operation{
 }
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
 struct LogicGate{
-    input_left: GateIO,
+    input_left: Option<GateIO>,
     input_right: Option<GateIO>,
     operation: Operation,
     value: Option<u32>
 }
 
 impl LogicGate{
-    pub fn new(input_left: GateIO, input_right: Option<GateIO>, operation: Operation) -> Self{
+    pub fn new(input_left: Option<GateIO>, input_right: Option<GateIO>, operation: Operation) -> Self{
         Self{
             input_left,
             input_right,
@@ -35,57 +35,6 @@ impl LogicGate{
         }
     }
 }
-
-// impl FromStr for LogicGate{
-//     type Err=String;
-
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         lazy_static!{
-//             static ref GATE_REGEX:Regex = Regex::new(r"([a-z]{1,2}|[0-9]+)? ?([A-Z]+)? ?([a-z]{1,2}|[0-9]+)? -> ([a-z]{1,2})").unwrap();
-//         }
-
-//         if let Some(captures) = GATE_REGEX.captures(s){
-//             // Pull out the Operation Name
-//             let mut operation = Operation::Nop; // make the default operation
-//             if let Some(op_str) = captures.get(2){
-//                 operation = match op_str.as_str(){
-//                     "AND" => Operation::And,
-//                     "OR" => Operation::Or,
-//                     "NOT" => Operation::Not,
-//                     "LSHIFT" => Operation::LShift,
-//                     "RSHIFT" => Operation::RShift,
-//                     x => return Err(format!("Could not Parse Operation from {}, got {}",s, x)),
-//                 };
-//             }
-
-//             // get the left input
-//             let left_input: GateIO;
-//             if let Some(x) = captures.get(1){
-//                 match x.as_str().parse::<u32>(){
-//                     Ok(val) => left_input = GateIO::Const(val),
-//                     Err(_) => {
-//                         // if it's a wire we need to see if it exists in the hash and assign that key
-//                         self.
-//                 }
-//             } else{
-//                 return Err(format!("could not parse circuit's left input"))
-//             }
-            
-//             // get the right input
-//             let right_input = match captures.get(3){
-//                 Some(x) => Some(GateIO::from_str(&x.as_str()).unwrap()),
-//                 None => None
-//             };
-
-//             // get the output
-//             let output = GateIO::from_str(&captures.get(4).ok_or_else(|| format!("Could not Extract Gate Output from {}",s))?.as_str()).unwrap();
-
-//             Ok(LogicGate::new(left_input, right_input, output, operation))
-//         } else {
-//             return Err(format!("Could not Parse String from {}",s))
-//         }
-//     }
-// }
 
 struct CircuitBoard{
     gates: Vec<Option<LogicGate>>,
@@ -116,7 +65,7 @@ impl CircuitBoard{
 
     pub fn insert(&mut self, gate_descriptor: &str) -> Result<(), String>{
         lazy_static!{
-            static ref GATE_REGEX:Regex = Regex::new(r"([a-z]{1,2}|[0-9]+)? ?([A-Z]+)? ?([a-z]{1,2}|[0-9]+)? -> ([a-z]{1,2})").unwrap();
+            static ref GATE_REGEX:Regex = Regex::new(r"^([a-z]{1,2}|[0-9]+)? ?([A-Z]+)? ?([a-z]{1,2}|[0-9]+)? -> ([a-z]{1,2})").unwrap();
         }
 
         if let Some(captures) = GATE_REGEX.captures(gate_descriptor){
@@ -136,16 +85,16 @@ impl CircuitBoard{
             };
 
             // get the left input
-            let left_input: GateIO;
+            let left_input=
             if let Some(x) = captures.get(1){
                 let input_str = x.as_str();
-                left_input = match input_str.parse::<u32>(){
-                    Ok(val) => GateIO::Const(val),
-                    Err(_) => GateIO::Wire(self.get_or_create_index(&input_str))
+                match input_str.parse::<u32>(){
+                    Ok(val) => Some(GateIO::Const(val)),
+                    Err(_) => Some(GateIO::Wire(self.get_or_create_index(&input_str)))
                 }
             } else{
-                return Err("could not parse circuit's left input".to_string())
-            }
+                None
+            };
             
             let right_input=
             if let Some(x) = captures.get(3){
@@ -177,41 +126,75 @@ impl CircuitBoard{
         }
     }
 
-    // pub fn measure_node(&mut self, node: &str) -> Result<u32, String>{
-    //     let gate_idx = *self.gate_lut.get(node).ok_or_else(|| format!("Node {} does not exist in Circuit",node))?;
-        
-    //     if let Some(x) = self.gates[gate_idx].value{
-    //         Ok(x)
-    //     } else{ // if it's none we have to process it
-    //         let l_input_value = match self.gates[gate_idx].input_left{
-    //             Some(GateIO::Const(x)) => x,
-    //             Some(GateIO::Wire(x)) => self.measure_node(&x)?, // ooh look recursion rears its ugly head!
-    //             None => return Err(format!("Gate {} has no left input!", node)), 
-    //         };
+    pub fn measure_node(&mut self, node: usize) -> Result<u32, String>{
+        let gate = self.gates[node].unwrap();
+        if let Some(x) = gate.value{
+            Ok(x)
+        } else{ // if it's none we have to process it
+            let l_input_value = match gate.input_left{
+                Some(GateIO::Const(x)) => Some(x),
+                Some(GateIO::Wire(x)) => Some(self.measure_node(x)?), // ooh look recursion rears its ugly head!
+                None => None
+            };
 
-    //         let r_input_value = match self.gates[gate_idx].input_right{
-    //             Some(GateIO::Const(x)) => Some(x),
-    //             Some(GateIO::Wire(x)) => Some(self.measure_node(&x)?),
-    //             None => None 
-    //         };
+            let r_input_value = match gate.input_right{
+                Some(GateIO::Const(x)) => Some(x),
+                Some(GateIO::Wire(x)) => Some(self.measure_node(x)?),
+                None => None 
+            };
             
-    //         let value = match self.gates[gate_idx].operation{
-    //             Operation::And => l_input_value & r_input_value.ok_or_else(|| String::from("Insufficient Inputs"))?,
-    //             Operation::LShift => l_input_value << 0x02,
-    //             Operation::RShift => l_input_value >> 0x02,
-    //             Operation::Or => l_input_value | r_input_value.ok_or_else(|| String::from("Insufficient Inputs"))?,
-    //             Operation::Not => !l_input_value,
-    //             Operation::Nop => l_input_value
-    //         };
+            let value = match gate.operation{
+                Operation::And => l_input_value.ok_or_else(|| String::from("Insufficient Inputs for AND"))?
+                     & r_input_value.ok_or_else(|| String::from("Insufficient Inputs Inputs for AND"))?,
+                Operation::LShift => l_input_value.ok_or_else(|| String::from("Insufficient Inputs for LSHIFT"))?
+                    << r_input_value.ok_or_else(|| String::from("Insufficient Inputs for LSHIFT"))?,
+                Operation::RShift => l_input_value.ok_or_else(|| String::from("Insufficient Inputs for RSHIFT"))?
+                     >> r_input_value.ok_or_else(|| String::from("Insufficient Inputs for RSHIFT"))?,
+                Operation::Or => l_input_value.ok_or_else(|| String::from("Insufficient Inputs for OR"))?
+                     | r_input_value.ok_or_else(|| String::from("Insufficient Inputs for OR"))?,
+                Operation::Not => !r_input_value.ok_or_else(|| String::from("Insufficient Inputs for NOT"))?,
+                Operation::Nop => l_input_value.ok_or_else(|| String::from("Insufficient Inputs for NOP"))?
+            };
 
-    //         self.gates[gate_idx].value = Some(value);
-    //         Ok(value)
-    //     }
-    // }
+            if let Some( ref mut x) = self.gates[node] { x.value = Some(value) };
+            Ok(value)
+        }
+    }
+
+    pub fn reset(&mut self){
+        for x in self.gates.iter_mut(){
+            if let Some(ref mut gate) = x { gate.value = None }
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("day7/input.txt").unwrap_or_else(|err| {
+        eprintln!("File Load Error {}", err);
+        process::exit(1);
+    });
+
+    let mut board = CircuitBoard::new();
+    input
+    .lines()
+    .try_for_each(|x| board.insert(x))
+    .unwrap_or_else(|err| {
+        eprintln!("File Load Error {}", err);
+        process::exit(1);
+    });
+
+    let idx = board.get_or_create_index("a");
+    let part_one_soln = board.measure_node(idx).unwrap();
+    println!("Part One Solution: {}", part_one_soln);
+
+    // for part 2 we reset the boards and change circuit b
+    board.reset();
+    let idx = board.get_or_create_index("b");
+    board.gates[idx] = Some(LogicGate::new(Some(GateIO::Const(part_one_soln)), None, Operation::Nop));
+
+    let idx = board.get_or_create_index("a");
+    let part_two_solution = board.measure_node(idx).unwrap();
+    println!("Part Two Solution: {}", part_two_solution);
 }
 
 
@@ -225,36 +208,46 @@ mod tests{
         assert_eq!(board.gates.len(),2);
         let index = board.get_or_create_index("bo");
         assert_eq!(board.gates[index].unwrap(), LogicGate::new(
-            GateIO::Wire(0),
+            Some(GateIO::Wire(0)),
             Some(GateIO::Const(2)),
             Operation::RShift
-        ))
+        ));
+
+        // if we insert an existing element the gate size shouldn't grow
+        board.insert("2 -> bn").unwrap();
+        let index = board.get_or_create_index("bn");
+        assert_eq!(board.gates.len(), 2);
+        assert_eq!(board.gates[index].unwrap(), LogicGate::new(
+            Some(GateIO::Const(2)),
+            None,
+            Operation::Nop,
+        ));
 
     }
 
-    // #[test]
-    // fn test_circuit_board_creation(){
-    //     let simple_circuit = r#"123 -> x
-    //     456 -> y
-    //     x AND y -> d
-    //     x OR y -> e
-    //     x LSHIFT 2 -> f
-    //     y RSHIFT 2 -> g
-    //     NOT x -> h
-    //     NOT y -> i"#;
+    #[test]
+    fn test_circuit_board_creation(){
+        let simple_circuit = r#"123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i"#;
 
-    //     // make a new circuit board
-    //     let mut board = CircuitBoard::new();
-    //     simple_circuit
-    //     .lines()
-    //     .map(|x| LogicGate::from_str(x))
-    //     .try_for_each(|x| {
-    //         board.insert(x?)
-    //     }).unwrap();
+        // make a new circuit board
+        let mut board = CircuitBoard::new();
+        simple_circuit
+        .lines()
+        .try_for_each(|x| {
+            board.insert(x)
+        }).unwrap();
 
-    //     // evaluate teh value of node y
-    //     assert_eq!(board.measure_node("y").unwrap(), 72);
-
-
-    // }
+        // evaluate teh value of node y
+        assert_eq!(board.gates.len(), 8);
+        let idx = board.get_or_create_index("d");
+        assert_eq!(board.measure_node(idx).unwrap(), 72);
+        assert_eq!(board.gates[idx].unwrap().value, Some(72));
+    }
 }
